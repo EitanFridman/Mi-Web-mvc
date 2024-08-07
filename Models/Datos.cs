3e7fd62c055f7a21afc12907b153862448bb737e
EITÁN GABRIEL FRIDMAN <48242472@ort>
2024-08-07 09:38:17 -0300

    commit 5

--- Models/Datos.cs
+++ Models/Datos.cs
@@ -6,7 +6,7 @@ namespace Mi_Web_mvc.Models
     {
         public static string Nombre = "Eitán Fridman";
         public static string Titulo = "Programador Web Full Stack";
-        public static string SobreMi = "Soy Eitán Fridman, estudiante de la escuela ORT en la especialidad de informática. Me encanta jugar al básquet y soy hincha fanático de Boca Juniors. Además de la tecnología, siempre me engancho en algún proyecto nuevo, y cuando no estoy en eso, seguro me encontrás en la cancha o hablando de la última jugada polémica del partido.";
+        public static string SobreMi = "Soy Eitán Fridman, estudiante de la escuela ORT en la especialidad de informática. Me encanta jugar al básquet y soy hincha fanático de Boca Juniors. Además de la tecnología, siempre me engancho en algún proyecto nuevo, y cuando no estoy en eso, seguro me encontrás en la cancha o hablando de la última jugada polémica del partido. Me gusta mucho viajar y conocer otros paises y culturas aunque mi pais favorito simpre va a ser Argentina. Una habilidad un poco inutil pero que aun asi me gusta es que me se todas las banderas de paises del mundo, tambien tengo otras como poder imitar el sonido de una gota o armar el cubo de rubik pero en mi opinion son menos divertidas.";
 
         public static List<Proyecto> Proyectos = new List<Proyecto>
         {
@@ -20,16 +20,16 @@ namespace Mi_Web_mvc.Models
             new Proyecto
             {
                 Titulo = "Proyecto 2",
-                Descripcion = "Este proyecto son 2 nav bars que se pueden usar de base para cualquier trabajo",
+                Descripcion = "Este proyecto son 2 navbars que se pueden usar de base para cualquier trabajo",
                 ImagenUrl = "/img/pc.png",
                 Link = "https://navbars-eitan-fridman.netlify.app/"
             },
             new Proyecto
             {
                 Titulo = "Proyecto 3",
-                Descripcion = "Proximamente",
-                ImagenUrl = "/img/signoDeRecarga.jpg",
-                Link = ""
+                Descripcion = "Esta es mi pequeña marca de ropa que ultimamente se puso de moda",
+                ImagenUrl = "/img/nike.jpg",
+                Link = "https://www.nike.com.ar/"
             },
             new Proyecto
             {
@@ -51,9 +51,9 @@ namespace Mi_Web_mvc.Models
         {
             new Certificado
             {
-                Institucion = "Universidad de Cambridge",
-                Titulo = "First Certificate",
-                Descripcion = "Certificado de nivel B2 ingles."
+                Institucion = "Macabi",
+                Titulo = "Madrij",
+                Descripcion = "Curso de lideres."
             },
             new Certificado
             {
